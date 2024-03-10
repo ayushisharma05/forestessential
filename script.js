e1dfa8baa7f78f9e37e4627dc2c2016ab42333e5
ayushisharma05 <146546987+ayushisharma05@users.noreply.github.com>
2024-03-10 21:52:20 +0530

    test

---
+++ script.js
@@ -0,0 +1,154 @@
+
+const products = document.querySelectorAll(".slider .product")
+let counter = 0;
+
+function left(){
+    if(counter == 0){
+        counter = products.length / 3 - 1;
+    }else{
+    counter--;
+    }
+    scroll();
+}
+function right(){
+    if(counter == products.length / 3 - 1){
+        counter = 0
+    }else{
+    counter++;
+    }
+    scroll();
+}
+function scroll(){
+    products.forEach(function(item){
+        item.style.transform = `translateX(-${counter * 1250}px)`
+})
+}
+
+let userTexts = document.getElementsByClassName("user-text");
+let userPics = document.getElementsByClassName("user-pic");
+
+function showReview(){
+    for(userPic of userPics){
+        userPic.classList.remove("active-pic");
+    }
+    for(userText of userTexts){
+        userText.classList.remove("active-text");
+    }
+    let i = Array.from(userPics).indexOf(event.target);
+    userPics[i].classList.add("active-pic");
+    userTexts[i].classList.add("active-text");
+}
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+});
+
+// function circleFlat(){
+//     //define default scale value
+//     var xscale = 1;
+//     var yscale = 1;
+
+//     var xprev = 0;
+//     var yprev = 0;
+
+//     window.addEventListener("mousemove", function (dets) {
+//         clearTimeout(timeout);
+//         xscale = gsap.utils.clamp(.9,1.5, dets.clientX - xprev);
+//         yscale = gsap.utils.clamp(.9,1.5, dets.clientY - yprev);
+
+//         xprev = dets.clientX;
+//         yprev = dets.clientY;
+
+//         circleMouseFollower(xscale,yscale);
+//         timeout = setTimeout(function () {
+//             document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`;
+//         },100);
+//     });
+// }
+// circleFlat();
+
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+let userMessage = null; // Variable to store user's message
+const API_KEY = "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"; // Paste your API key here
+const inputInitHeight = chatInput.scrollHeight;
+const createChatLi = (message, className) => {
+    // Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
+const generateResponse = (chatElement) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = chatElement.querySelector("p");
+    // Define the properties and message for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}],
+        })
+    }
+    // Send POST request to API, get response and set the reponse as paragraph text
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content.trim();
+    }).catch(() => {
+        messageElement.classList.add("error");
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+    if(!userMessage) return;
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    
+    setTimeout(() => {
+        // Display "Thinking..." message while waiting for the response
+        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+function function1(){
+    swal("CURRENCY!", "$USD")
+}
+
+document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
